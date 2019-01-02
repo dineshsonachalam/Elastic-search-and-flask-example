d61b9fc80183941a61c6577fdfc2350ce52f77c7
dineshsonachalam <dineshsonachalam@gmail.com>
2019-01-02 14:59:44 +0530

    Elastic search and flask

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+es = Elasticsearch('localhost', port=9200)
+
+@app.route('/')
+def home():
+    return render_template('search.html')
+
+@app.route('/search/results', methods=['GET', 'POST'])
+def search_request():
+    search_term = request.form["input"]
+    res = es.search(
+        index="blog-sysadmins", 
+        size=20, 
+        body={
+            "query": {
+                "multi_match" : {
+                    "query": search_term, 
+                    "fields": [
+                        "url", 
+                        "title", 
+                        "tags"
+                    ] 
+                }
+            }
+        }
+    )
+    print("Results:",res)
+    return render_template('results.html', res=res )
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(host='0.0.0.0', port=5000)
---
+++ readme.md
@@ -0,0 +1,2 @@
+### References:
+1. https://sysadmins.co.za/building-a-search-engine-for-our-scraped-data-on-elasticsearch-part-2/
